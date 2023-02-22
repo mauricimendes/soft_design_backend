1b17c670dd5b6e02bfa0c57c35cf4221f1ab29a8
mauricimendes <mauricimendes.14@gmail.com>
2023-02-22 19:51:45 -0300

    add tests for details book

--- src/modules/books/services/DetailsBookService.spec.ts
+++ src/modules/books/services/DetailsBookService.spec.ts
@@ -1,12 +1,12 @@
 import FakeHashProvider from '@modules/users/providers/HashProvider/fakes/FakeHashProvider'
 import FakeUsersRepository from '@modules/users/repositories/fakes/FakeUsersRepository'
 import FakeBooksRepository from '../repositories/fakes/FakeBooksRepository'
-import FindAllBookService from './FindAllBookService'
+import DetailsBookService from './DetailsBookService'
 
 let fakeBooksRepository: FakeBooksRepository
 let fakeUsersRepository: FakeUsersRepository
 let fakeHashProvider: FakeHashProvider
-let findAll: FindAllBookService
+let details: DetailsBookService
 
 describe('FindAllBooks', () => {
   beforeEach(() => {
@@ -14,12 +14,12 @@ describe('FindAllBooks', () => {
     fakeUsersRepository = new FakeUsersRepository()
     fakeHashProvider = new FakeHashProvider()
 
-    findAll = new FindAllBookService(
+    details = new DetailsBookService(
       fakeBooksRepository
     )
   })
 
-  it('should be able to list the books without filters', async () => {
+  it('should be able to show details a book', async () => {
     const user = await fakeUsersRepository.create({
       name: 'Jonh Doe',
       email: 'johndoe@example.com',
@@ -64,47 +64,8 @@ describe('FindAllBooks', () => {
       ]
     })
 
-    const books = await findAll.execute()
+    const book = await details.execute(book1._id.toString())
 
-    expect(books).toEqual([book1, book2, book3])
+    expect(book).toEqual(book1)
   })
-
-  it('should be able to list the books with filters', async () => {
-    const user = await fakeUsersRepository.create({
-      name: 'Jonh Doe',
-      email: 'johndoe@example.com',
-      phone: '(99) 12345-6789',
-      password: '123456789',
-      is_admin: true
-    })
-
-    const book1 = await fakeBooksRepository.create({
-      title: 'Harry Potter e a pedra filosofal',
-      author: 'J.K. Rowling',
-      number_pages: 206,
-      synopsis: 'Synopsis of book and your descriptions',
-      created_by_admin: String(user._id),
-      images: [
-        { filename: 'capa.jpg' },
-        { filename: 'verso.jpg' }
-      ]
-    })
-
-    const book2 = await fakeBooksRepository.create({
-      title: 'Revolução dos bichos',
-      author: 'George Orwell',
-      number_pages: 206,
-      synopsis: 'Synopsis of book and your descriptions',
-      created_by_admin: String(user._id),
-      images: [
-        { filename: 'capa.jpg' },
-        { filename: 'verso.jpg' }
-      ]
-    })
-
-    const books = await findAll.execute('Harry')
-
-    expect(books).toEqual([book1])
-  })
-
 })
