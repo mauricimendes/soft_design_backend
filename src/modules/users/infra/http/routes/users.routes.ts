9df009284e083c5f186d69640a9a29ed6a27c30d
mauricimendes <mauricimendes.14@gmail.com>
2023-02-22 16:39:10 -0300

    add validation in routes of users

--- src/modules/users/infra/http/routes/users.routes.ts
+++ src/modules/users/infra/http/routes/users.routes.ts
@@ -1,3 +1,4 @@
+import { celebrate, Joi, Segments } from 'celebrate'
 import { Router } from 'express'
 import UsersController from '../controllers/UsersController'
 
@@ -5,6 +6,14 @@ const usersRouter = Router()
 
 const usersController = new UsersController()
 
-usersRouter.post('/', usersController.store)
+usersRouter.post('/', celebrate({
+  [Segments.BODY]: {
+    name: Joi.string().required(),
+    email: Joi.string().required(),
+    password: Joi.string().required(),
+    phone: Joi.string().required(),
+    is_admin: Joi.bool(),
+  }
+}), usersController.store)
 
 export default usersRouter
