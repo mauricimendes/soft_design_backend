dd4a3e2b046410260df01078ef511bf6cffcfd26
mauricimendes <mauricimendes.14@gmail.com>
2023-02-22 16:37:06 -0300

    add validation in routes of rents

--- src/modules/rents/infra/http/routes/rents.routes.ts
+++ src/modules/rents/infra/http/routes/rents.routes.ts
@@ -2,11 +2,21 @@ import { Router } from 'express'
 
 import ensureAuthenticated from '@modules/users/infra/http/middlewares/ensureAuthenticated'
 import RentsController from '../controllers/RentsController'
+import { celebrate, Joi, Segments } from 'celebrate'
 
 const rentsRouter = Router()
 const rentsController = new RentsController()
 
-rentsRouter.post('/:id', ensureAuthenticated, rentsController.store)
-rentsRouter.patch('/:id', ensureAuthenticated, rentsController.update)
+rentsRouter.post('/:id', ensureAuthenticated, celebrate({
+  [Segments.PARAMS]: {
+    id: Joi.string().required()
+  },
+}), rentsController.store)
+
+rentsRouter.patch('/:id', ensureAuthenticated, celebrate({
+  [Segments.PARAMS]: {
+    id: Joi.string().required()
+  },
+}), rentsController.update)
 
 export default rentsRouter
